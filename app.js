4297ba382d183e501744ca8a90eedda2010b802a
Sergey Litvinov <litvinows9@gmail.com>
2022-08-21 11:59:28 +0300

    fix eslint err

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-
 
 const { PORT = 3000 } = process.env;
 const app = express();
