b40258fc9d0bdda2d21cd8d6c08e8d0001ca44e9
Sergey Litvinov <litvinows9@gmail.com>
2022-08-21 12:33:52 +0300

    create user model

---
+++ models/user.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String, // строка
+    minlength: [2, 'Должно быть от 2 до 30 символов'], // минимальная длина имени — 2 символа
+    maxlength: [30, 'Должно быть от 2 до 30 символов'], // а максимальная — 30 символов
+    default: 'Жак-Ив Кусто',
+  },
+  email: {
+    type: String,
+    required: [true, 'Введите email'],
+    unique: true,
+    validate: {
+      validator: (email) => validator.isEmail(email),
+      message: 'Введен некорректный email',
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Введите пароль'],
+    select: false,
+  },
+}, { versionKey: false });
+
+module.exports = mongoose.model('User', userSchema);
