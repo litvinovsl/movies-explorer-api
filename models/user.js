5e4b0185b106ff5b861269b0661fd2b238c6c562
Sergey Litvinov <litvinows9@gmail.com>
2022-08-28 14:20:26 +0300

    used Unauthorized error

--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
 const bcrypt = require('bcrypt');
+const UnauthorizedError = require('../errors/unauthorized-err');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -28,13 +29,13 @@ userSchema.statics.findUserByCredentials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (user === null) {
-        // throw new UnauthorizedError('Неверная почта или пароль');
+        throw new UnauthorizedError('Неверная почта или пароль');
       }
 
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            // throw new UnauthorizedError('Неверная почта или пароль');
+            throw new UnauthorizedError('Неверная почта или пароль');
           }
 
           return user;
